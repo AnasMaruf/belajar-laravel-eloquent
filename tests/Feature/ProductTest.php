7057061e29a6cf1ef7309421011e6bcf65964395
AnasMaruf <anasmaruf1101@gmail.com>
2025-01-02 11:48:18 +0700

    Has one of many

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\Product;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testOneToMany()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $product = Product::find("1");
+        self::assertNotNull($product);
+
+        $category = $product->category;
+        self::assertNotNull($category);
+        self::assertEquals("FOOD", $category->id);
+
+    }
+
+    public function testHasOneOfMany()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::find("FOOD");
+        self::assertNotNull($category);
+
+        $cheapestProduct = $category->cheapestProduct;
+        self::assertNotNull($cheapestProduct);
+        self::assertEquals("1", $cheapestProduct->id);
+
+        $mostExpensiveProduct = $category->mostExpensiveProduct;
+        self::assertNotNull($mostExpensiveProduct);
+        self::assertEquals("2", $mostExpensiveProduct->id);
+    }
+}
